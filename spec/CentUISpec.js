6affbdd987133b141c68a72725af7f205dde541a
Kristi Centinaro <kristi@theresnobox.net>
2012-09-23 18:52:19 -0400

    Loads of front-end work

---
+++ spec/CentUISpec.js
@@ -0,0 +1,22 @@
+describe("Cent UI", function(){
+  var CentUI;
+
+  beforeEach(function(){
+    // make a div to plop the profiles.
+    $('#jasmine_content').append('.profileBlocks');
+
+    CentUI = new Cent.UI({
+        "parentContainer": '#jasmine_content',
+        "profiles": ".profileBlocks"
+      });
+  });
+
+  afterEach(function (){
+    $('#jasmine_content').empty();
+  });
+
+  it("On init, load the data from centinaro.json", function(){
+    expect(CentUI.siteContent["kristi"].name).toEqual("Kristi Centinaro");
+  });
+
+});
