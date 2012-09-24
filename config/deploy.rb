f3faab9160e059c301818df2d9c26990dde79a83
Bill <bill@theresnobox.net>
2012-09-24 13:15:47 -0400

    Fixed cap script

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,7 +13,7 @@ role :web, "198.101.243.95"                          # Your HTTP server, Apache/
 role :app, "198.101.243.95"                          # This may be the same as your `Web` server
 
 # if you want to clean up old releases on each deploy uncomment this:
-after "deploy:restart", "deploy:cleanup"
+#after "deploy:restart", "deploy:cleanup"
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
